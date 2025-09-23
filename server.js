eea6fab0d659bc268ca73a150b579956ed9961f9
Raamprathap <raamprathap17242@gmail.com>
2025-09-23 19:18:25 +0530

    Added CORS

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//handle CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
